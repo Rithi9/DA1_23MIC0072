486ca466d636dfd0101f333e35dd54695b7f47bb
Rithii9 <rithii9@gmail.com>
2026-01-11 18:53:45 +0530

    First commit - Java program

---
+++ DA1_23MIC0072.java
@@ -0,0 +1,7 @@
+public class DA1_23MIC0072{
+    public static void main(String[] args) {
+        System.out.println("Student Name: RITHIKA M");
+        System.out.println("Register Number: 23MIC0072");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
