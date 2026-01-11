86aade27216b022b414006b8fe2ceefc398a1704
Rithii9 <rithii9@gmail.com>
2026-01-11 19:15:52 +0530

    Second commit - Java program

--- DA1_23MIC0072.java
+++ DA1_23MIC0072.java
@@ -3,5 +3,6 @@ public class DA1_23MIC0072{
         System.out.println("Student Name: RITHIKA M");
         System.out.println("Register Number: 23MIC0072");
         System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("Second Commit");
     }
 }
